66bcabb66f624df162f15b6088ee1923cef16059
laszlocaballero <laszlocaballero@gmail.com>
2024-10-03 03:07:18 -0500

    fix

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import math
 
 def sumar(a,b):
-    return a+b;
+    return a+b
 
 def restar(self, a, b):
         return a - b
 
 def multiplicar(a,b):
-    return a * b;
+    return a * b
 
 def dividir(a,b):
-    return a/b;
+    return a/b
 
 def raiz(a):
     return math.sqrt(a)
